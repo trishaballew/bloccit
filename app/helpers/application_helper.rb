0d93ad71475669e889d982e98ad0b629ac530766
trishaballew <trishaballew@gmail.com>
2015-02-17 19:54:00 -0500

    Validating posts 3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+  def my_name
+    "Trish"
+  end
   def form_group_tag(errors, &block)
     if errors.any?
       content_tag :div, capture(&block), class: 'form-group has-error'
