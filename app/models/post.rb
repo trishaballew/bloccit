9d4e76b2c8d1c8040db57ec4d465558bbb1578cb
trishaballew <trishaballew@gmail.com>
2015-04-27 21:56:44 -0400

    voting checkpoint

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
 
-  default_scope { order('created_at DESC') }
+  default_scope { order('rank DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
@@ -43,5 +43,10 @@ private
     (redcarpet.render markdown).html_safe
   end
 
-
+   def update_rank
+     age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
+     new_rank = points + age_in_days
+ 
+     update_attribute(:rank, new_rank)
+   end
 end
