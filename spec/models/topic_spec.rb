f8f5652359b16482f33011665f785b12209514e0
trishaballew <trishaballew@gmail.com>
2015-05-24 11:09:15 -0400

    private topics assignment

---
+++ spec/models/topic_spec.rb
@@ -0,0 +1,34 @@
+describe Topic do |variable|
+  describe "scopes" do
+
+    before do
+      @public_topic = Topic.create # default is public
+      @private_topic = Topic.create(public: false)
+    end
+
+    describe "publicly_viewable" do
+      it "returns a relation of all public topics" do
+        expect(Topic.publicly_viewable).to eq( [@public_topic] )
+      end
+    end
+
+    describe "privately_viewable" do
+      it "returns a relation of all private topics" do
+        expect(Topic.privately_viewable).to eq( [@private_topic] )
+      end
+    end
+
+    describe "visible_to(user)" do
+      it "returns all topics if the user is present" do
+        user = true 
+        expect(Topic.visible_to(user)).to eq(Topic.all)
+      end
+    
+
+      it "returns only public specs if user is nil" do
+        expect(Topic.visible_to(nil)).to eq(Topic.public)
+      end
+    end
+
+   end
+  end
