6173e2a07dbe9948b7df4b0f84d1191afdd908ba
trishaballew <trishaballew@gmail.com>
2015-05-30 21:21:06 -0400

    another-interlude-assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,9 +8,15 @@ module ApplicationHelper
   end
 
   def down_vote_link_classes(post)
+    #"glyphicon glyphicon-chevron-down "
+    #current_user.voted(post)
+    # "voted" if vote && vote.down_vote?
   end
 
   def up_vote_link_classes(post)
+  #"glyphicon glyphicon-chevron-up "
+  #current_user.voted(post)
+  #"voted" if vote && vote.up_vote?
   end
 
 end
