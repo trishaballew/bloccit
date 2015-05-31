42c263d08ff66f4157f3e18194a0e427892bc6ee
trishaballew <trishaballew@gmail.com>
2015-05-30 22:53:44 -0400

    public profile checkpoint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
 
   def show
-    
+    @user = User.find(params[:id])
+    @posts = @user.posts.visible_to(current_user)
+    @comments = @user.comments
   end
 
   def update
